cf956691e4e9a0ccd6c7aaf8981bbeaa25424620
Tatsuya Ishii <tishii2479@gmail.com>
2021-04-14 15:57:47 +0900

    Initial commit

---
+++ SimpleClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleClock
+//
+//  Created by Tatsuya Ishii on 2021/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleClock/SimpleClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleClockApp.swift
+//  SimpleClock
+//
+//  Created by Tatsuya Ishii on 2021/04/14.
+//
+
+import SwiftUI
+
+@main
+struct SimpleClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
